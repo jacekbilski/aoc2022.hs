66659be30ecc55858598a7967c0c817035a91afa
Jacek Bilski <jacek@bilski.tech>
2022-12-19 13:45:55 +0100

    Better naming and finally some (example) code.

---
+++ app/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import qualified AoC2022 (someFunc)
+
+main :: IO ()
+main = do
+  putStrLn "Hello, Advent of Code 2022!"
+  AoC2022.someFunc
---
+++ src/AoC2022.hs
@@ -0,0 +1,4 @@
+module AoC2022 (someFunc) where
+
+someFunc :: IO ()
+someFunc = putStrLn "Day 1 of AoC 2022"
