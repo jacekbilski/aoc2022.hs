78382edd9f2be150e9b256fcc330e2eb0b555351
Jacek Bilski <jacek@bilski.tech>
2022-12-19 16:48:47 +0100

    Very strange, it's working correctly!

--- src/AoC2022.hs
+++ src/AoC2022.hs
@@ -6,9 +6,19 @@ import Control.Exception
 day01 :: IO ()
 day01 = bracket (openFile "inputs/day01.input1" ReadMode) hClose
                 (\h -> do contents <- hGetContents h
-                          sumCalories contents)
+                          let maxCalories = findMaxCalories contents
+                          putStrLn (show maxCalories))
 
-sumCalories :: String -> IO ()
-sumCalories input = do
-  putStrLn "The first 10 lines:"
-  mapM_ putStrLn (take 10 (lines input))
+-- Basic idea: single go through the input
+-- in accumulator I keep: current max calories of some elf I processed already and sum of calories for current elf
+-- folding: in case element is a number -> increase sum of calories for the current given elf
+--  in case element is empty or no more elements -> take highest from the values in the tuple
+
+newValue :: String -> (Integer, Integer) -> (Integer, Integer)
+newValue "" (currMax, curr) = (maximum [currMax, curr], 0)
+newValue x (currMax, curr) = (currMax, curr + (read x))
+
+findMaxCalories :: String -> Integer
+findMaxCalories input = do
+  let acc = foldr newValue (0, 0) (lines input)
+  maximum [(fst acc), (snd acc)]
