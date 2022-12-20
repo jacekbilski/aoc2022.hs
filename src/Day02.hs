43f79fec34bbdd4f0061844337b6501b5604c231
Jacek Bilski <jacek@bilski.tech>
2022-12-20 16:34:53 +0100

    Switching to foldl1 and case expressions.

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,17 +1,18 @@
 module Day02 (day02_1) where
 
 day02_1 :: String -> Integer
-day02_1 input = foldr (+) 0 (map score (lines input))
+day02_1 input = foldl1 (+) (map score (lines input))
 
 score :: String -> Integer
+score round = case round of
 -- outcome + shape I selected
-score "A X" = 3 + 1
-score "A Y" = 6 + 2
-score "A Z" = 0 + 3
-score "B X" = 0 + 1
-score "B Y" = 3 + 2
-score "B Z" = 6 + 3
-score "C X" = 6 + 1
-score "C Y" = 0 + 2
-score "C Z" = 3 + 3
-score _ = 0
+  "A X" -> 3 + 1
+  "A Y" -> 6 + 2
+  "A Z" -> 0 + 3
+  "B X" -> 0 + 1
+  "B Y" -> 3 + 2
+  "B Z" -> 6 + 3
+  "C X" -> 6 + 1
+  "C Y" -> 0 + 2
+  "C Z" -> 3 + 3
+  _ -> error "invalid round"
