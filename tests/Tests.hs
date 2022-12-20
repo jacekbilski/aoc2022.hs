83881ca7ea8f461dd2dbeaedd6e41638608381eb
Jacek Bilski <jacek@bilski.tech>
2022-12-20 14:16:24 +0100

    Added tests. Why cabal says "Test suite test-aoc2022: PASS", but in the log it's clearly "Cases: 2  Tried: 2  Errors: 0  Failures: 2".

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,7 +1,20 @@
 module Main where
 
-import System.Exit (exitFailure)
+import Test.HUnit
+import qualified AoC2022 (solve, day01_1, day01_2)
 
-main = do
-    putStrLn "This test always fails!"
-    exitFailure
+test_day01_1 :: Test
+test_day01_1 = TestCase (do
+  result <- AoC2022.solve "inputs/day01.input" AoC2022.day01_1
+  assertEqual "It's 69206" 69216 result)
+
+test_day01_2 :: Test
+test_day01_2 = TestCase (do
+  result <- AoC2022.solve "inputs/day01.input" AoC2022.day01_2
+  assertEqual "It's 197400" 197401 result)
+
+tests :: Test
+tests = TestList [TestLabel "day 01 part 1" test_day01_1, TestLabel "day 01 part 2" test_day01_2]
+
+main :: IO Counts
+main = runTestTT tests
