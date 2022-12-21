fabf41a9b89863224de550a3da55f1f44f7319b8
Jacek Bilski <jacek@bilski.tech>
2022-12-21 15:06:49 +0100

    Day 05 - infrastructure.

--- src/AoC2022.hs
+++ src/AoC2022.hs
@@ -1,4 +1,4 @@
-module AoC2022 (solve, day01_1, day01_2, day02_1, day02_2, day03_1, day03_2, day04_1, day04_2) where
+module AoC2022 (solve, day01_1, day01_2, day02_1, day02_2, day03_1, day03_2, day04_1, day04_2, day05_1, day05_2) where
 
 import Control.Exception
 import Flow
@@ -8,6 +8,7 @@ import Day01
 import Day02
 import Day03
 import Day04
+import Day05
 
 solve :: (Show a) => FilePath -> ([String] -> a) -> IO a
 solve fileName day = do
---
+++ src/Day05.hs
@@ -0,0 +1,7 @@
+module Day05 (day05_1, day05_2) where
+
+day05_1 :: [String] -> String
+day05_1 input = undefined
+
+day05_2 :: [String] -> String
+day05_2 input = undefined
