1b0c780adbf63f003b074598d29d2b14c84c126a
Jacek Bilski <jacek@bilski.tech>
2023-01-02 16:27:56 +0100

    Day 05 part 2 - preparatory refactorings.

--- src/Day05.hs
+++ src/Day05.hs
@@ -8,13 +8,17 @@ import Flow
 
 type Stacks = Map Char [Char]
 type Step = (Int, Char, Char) -- quantity, source, target
+type Procedure = [Step] -> Stacks -> Stacks
 
 day05_1 :: [String] -> String
-day05_1 input = do
+day05_1 input = rearrange_stacks do_apply_procedure_9000 input
+
+rearrange_stacks :: Procedure -> [String] -> String
+rearrange_stacks procedure input = do
   let separator_line_index = zip [0..] input |> filter (\i -> snd i == "") |> head |> fst
   let stacks_input = take separator_line_index input
   let procedure_input = drop (separator_line_index + 1) input
-  load_stacks stacks_input |> apply_procedure procedure_input |> Map.map head |> Map.elems
+  load_stacks stacks_input |> apply_procedure procedure procedure_input |> Map.map head |> Map.elems
 
 load_stacks :: [String] -> Stacks
 load_stacks input = do
@@ -25,25 +29,28 @@ load_stacks input = do
 take_col :: [[Char]] -> Int -> [Char]
 take_col tab col = map (\r -> r !! col) tab
 
-apply_procedure :: [String] -> Stacks -> Stacks
-apply_procedure input stacks = do
-  let procedure = parse_procedure input
-  do_apply_procedure procedure stacks
+apply_procedure :: Procedure -> [String] -> Stacks -> Stacks
+apply_procedure procedure input stacks = do
+  let steps = parse_procedure input
+  procedure steps stacks
 
 parse_procedure :: [String] -> [Step]
 parse_procedure input = input |> map words |> map (\a -> (read (a !! 1), a !! 3 !! 0, a !! 5 !! 0))
 
-do_apply_procedure :: [Step] -> Stacks -> Stacks
-do_apply_procedure [] stacks = stacks
-do_apply_procedure steps stacks = do
+do_apply_procedure_9000 :: Procedure
+do_apply_procedure_9000 [] stacks = stacks
+do_apply_procedure_9000 steps stacks = do
   let step = head steps
   let (quantity, source, target) = step
   if quantity == 0
     then do
-      do_apply_procedure (tail steps) stacks
+      do_apply_procedure_9000 (tail steps) stacks
     else do
       let e = stacks Map.! source |> head
-      Map.adjust ([e] ++) target stacks |> Map.adjust (tail) source |> do_apply_procedure ((quantity - 1, source, target) : (tail steps))
+      Map.adjust ([e] ++) target stacks |> Map.adjust (tail) source |> do_apply_procedure_9000 ((quantity - 1, source, target) : (tail steps))
 
 day05_2 :: [String] -> String
-day05_2 input = undefined
+day05_2 input = rearrange_stacks do_apply_procedure_9001 input
+
+do_apply_procedure_9001 :: Procedure
+do_apply_procedure_9001 steps stacks = stacks
