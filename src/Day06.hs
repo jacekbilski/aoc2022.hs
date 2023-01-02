6f5c54846702e2fda6fdeef89d662af56ed46e9b
Jacek Bilski <jacek@bilski.tech>
2023-01-02 17:42:13 +0100

    Day 06 part 2. Using sets, like everyone else.

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,6 +1,7 @@
 module Day06 (day06_1, day06_2) where
 
 import Flow
+import qualified Data.Set as Set
 
 day06_1 :: [String] -> Int
 day06_1 input = head input |> find_start_of_x 4 4
@@ -9,9 +10,7 @@ find_start_of_x :: Int -> Int -> String -> Int
 find_start_of_x marker_size to_skip datastream = if (all_different (take marker_size datastream)) then to_skip else find_start_of_x marker_size (to_skip + 1) (tail datastream)
 
 all_different :: [Char] -> Bool
-all_different [] = True
-all_different (_:[]) = True
-all_different (x:xs) = ((filter (\e -> e == x) xs |> length) == 0) && all_different xs
+all_different chars = (Set.fromList chars |> Set.size) == length chars
 
 day06_2 :: [String] -> Int
 day06_2 input = head input |> find_start_of_x 14 14
