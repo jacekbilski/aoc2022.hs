412895e4cf618c7f2ca9a7f9cd423034dc7d3252
Jacek Bilski <jacek@bilski.tech>
2023-01-03 11:01:54 +0100

    Why underscores?

--- src/Day05.hs
+++ src/Day05.hs
@@ -11,55 +11,55 @@ type Step = (Int, Char, Char) -- quantity, source, target
 type Procedure = [Step] -> Stacks -> Stacks
 
 day05_1 :: [String] -> String
-day05_1 input = rearrange_stacks use_crate_mover_9000 input
+day05_1 input = rearrangeStacks useCrateMover9000 input
 
-rearrange_stacks :: Procedure -> [String] -> String
-rearrange_stacks procedure input = do
-  let separator_line_index = zip [0..] input |> filter (\i -> snd i == "") |> head |> fst
-  let stacks_input = take separator_line_index input
-  let procedure_input = drop (separator_line_index + 1) input
-  load_stacks stacks_input |> apply_procedure procedure procedure_input |> Map.map head |> Map.elems
+rearrangeStacks :: Procedure -> [String] -> String
+rearrangeStacks procedure input = do
+  let separatorLineIndex = zip [0..] input |> filter (\i -> snd i == "") |> head |> fst
+  let stacksInput = take separatorLineIndex input
+  let procedureInput = drop (separatorLineIndex + 1) input
+  loadStacks stacksInput |> applyProcedure procedure procedureInput |> Map.map head |> Map.elems
 
-load_stacks :: [String] -> Stacks
-load_stacks input = do
-  let stack_input_indexes = zip [0..] (last input) |> filter (\c -> snd c /= ' ') |> map (\t -> fst t)
-  let stacks_input = init input
-  map (\s -> ((last input) !! s, take_col stacks_input s |> filter (\c -> c /= ' '))) stack_input_indexes |> Map.fromList
+loadStacks :: [String] -> Stacks
+loadStacks input = do
+  let stacksInputIndexes = zip [0..] (last input) |> filter (\c -> snd c /= ' ') |> map (\t -> fst t)
+  let stacksInput = init input
+  map (\s -> ((last input) !! s, takeCol stacksInput s |> filter (\c -> c /= ' '))) stacksInputIndexes |> Map.fromList
 
-take_col :: [[Char]] -> Int -> [Char]
-take_col tab col = map (\r -> r !! col) tab
+takeCol :: [[Char]] -> Int -> [Char]
+takeCol tab col = map (\r -> r !! col) tab
 
-apply_procedure :: Procedure -> [String] -> Stacks -> Stacks
-apply_procedure procedure input stacks = do
-  let steps = parse_procedure input
+applyProcedure :: Procedure -> [String] -> Stacks -> Stacks
+applyProcedure procedure input stacks = do
+  let steps = parseProcedure input
   procedure steps stacks
 
-parse_procedure :: [String] -> [Step]
-parse_procedure input = input |> map words |> map (\a -> (read (a !! 1), a !! 3 !! 0, a !! 5 !! 0))
+parseProcedure :: [String] -> [Step]
+parseProcedure input = input |> map words |> map (\a -> (read (a !! 1), a !! 3 !! 0, a !! 5 !! 0))
 
-use_crate_mover_9000 :: Procedure
-use_crate_mover_9000 [] stacks = stacks
-use_crate_mover_9000 steps stacks = do
+useCrateMover9000 :: Procedure
+useCrateMover9000 [] stacks = stacks
+useCrateMover9000 steps stacks = do
   let step = head steps
   let (quantity, source, target) = step
   if quantity == 0
     then do
-      use_crate_mover_9000 (tail steps) stacks
+      useCrateMover9000 (tail steps) stacks
     else do
       let e = stacks Map.! source |> head
-      Map.adjust ([e] ++) target stacks |> Map.adjust (tail) source |> use_crate_mover_9000 ((quantity - 1, source, target) : (tail steps))
+      Map.adjust ([e] ++) target stacks |> Map.adjust (tail) source |> useCrateMover9000 ((quantity - 1, source, target) : (tail steps))
 
 day05_2 :: [String] -> String
-day05_2 input = rearrange_stacks use_crate_mover_9001 input
+day05_2 input = rearrangeStacks useCrateMover9001 input
 
-use_crate_mover_9001 :: Procedure
-use_crate_mover_9001 [] stacks = stacks
-use_crate_mover_9001 steps stacks = do
+useCrateMover9001 :: Procedure
+useCrateMover9001 [] stacks = stacks
+useCrateMover9001 steps stacks = do
   let step = head steps
   let (quantity, source, target) = step
   if quantity == 0
     then do
-      use_crate_mover_9001 (tail steps) stacks
+      useCrateMover9001 (tail steps) stacks
     else do
       let es = stacks Map.! source |> take quantity
-      Map.adjust (es ++) target stacks |> Map.adjust (drop quantity) source |> use_crate_mover_9001 (tail steps)
+      Map.adjust (es ++) target stacks |> Map.adjust (drop quantity) source |> useCrateMover9001 (tail steps)
--- src/Day06.hs
+++ src/Day06.hs
@@ -4,13 +4,13 @@ import Flow
 import qualified Data.Set as Set
 
 day06_1 :: [String] -> Int
-day06_1 input = head input |> find_start_of_x 4 4
+day06_1 input = head input |> findStartOfX 4 4
 
-find_start_of_x :: Int -> Int -> String -> Int
-find_start_of_x marker_size to_skip datastream = if (all_different (take marker_size datastream)) then to_skip else find_start_of_x marker_size (to_skip + 1) (tail datastream)
+findStartOfX :: Int -> Int -> String -> Int
+findStartOfX markerSize toSkip dataStream = if (allDifferent (take markerSize dataStream)) then toSkip else findStartOfX markerSize (toSkip + 1) (tail dataStream)
 
-all_different :: [Char] -> Bool
-all_different chars = (Set.fromList chars |> Set.size) == length chars
+allDifferent :: [Char] -> Bool
+allDifferent chars = (Set.fromList chars |> Set.size) == length chars
 
 day06_2 :: [String] -> Int
-day06_2 input = head input |> find_start_of_x 14 14
+day06_2 input = head input |> findStartOfX 14 14
