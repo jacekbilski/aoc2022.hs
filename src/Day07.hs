38064981233db78409684b55618e0b1577e42d73
Jacek Bilski <jacek@bilski.tech>
2023-01-05 14:47:58 +0100

    Messing around part 6 - Maybe RegularFile -> [RegularFile]

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,13 +1,12 @@
 module Day07 (day07_1, day07_2) where
 
-import Data.List (stripPrefix)
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
+--import Data.Map.Strict (Map)
+--import qualified Data.Map.Strict as Map
 import Flow
 
 --data File = RegularFile String Int | Directory String [File]
 --type Directory = Map String [RegularFile]  -- name, files
-type Directory = (String, Maybe RegularFile)
+type Directory = (String, [RegularFile])
 type RegularFile = (String, Int)  -- name, size
 
 day07_1 :: [String] -> Int
@@ -17,13 +16,13 @@ day07_1 input = do
 
 buildDirectoryTree :: [String] -> Directory
 --buildDirectoryTree = doBuildDirectoryTree (Map.fromList [("/", [])]) []
-buildDirectoryTree = doBuildDirectoryTree ("/", Nothing) []
+buildDirectoryTree = doBuildDirectoryTree ("/", []) []
 
 doBuildDirectoryTree :: Directory -> [Directory] -> [String] -> Directory -- root -> current path -> input -> final directory tree
 doBuildDirectoryTree root _ [] = root
 doBuildDirectoryTree root path (x:xs)
   | x == "$ cd /" = do  -- I do hope this is just the first command and happens just once
-    let newRoot = ("/", Nothing)
+    let newRoot = ("/", [])
     doBuildDirectoryTree newRoot [newRoot] xs
   | x == "$ ls" = doBuildDirectoryTree root path xs -- nothing to do
 --  | x == "$ cd .." = doBuildDirectoryTree root (tail path) xs
@@ -35,7 +34,7 @@ doBuildDirectoryTree root path (x:xs)
       let fs = read (head y) :: Int
       let filename = y !! 1
       let file = (filename, fs)
-      let newRoot = ("/", Just file)
+      let newRoot = ("/", [file])
       doBuildDirectoryTree newRoot [newRoot] xs
 
 --  | Just dirName <- stripPrefix "$ cd " x = doBuildDirectoryTree root ((head path) !! dirName) xs
@@ -48,8 +47,7 @@ findDirs dir = [dir]
 --findDirs (RegularFile _ _) = []
 
 size :: Directory -> Int
-size (_, file) | Just f <- file = snd f
-               | otherwise = 0
+size (_, files) = map snd files |> sum
 --size dir = Map.elems dir |> concat |> map snd |> sum
 --size (Directory name [files]) = 0
 --size (RegularFile _ s) = s
