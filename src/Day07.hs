45a924402ea4ceb896dd570b6e28318dfb7a2022
Jacek Bilski <jacek@bilski.tech>
2023-01-05 15:00:40 +0100

    Messing around part 7 - Switching to Map, dumping RegularFile.

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,13 +1,13 @@
 module Day07 (day07_1, day07_2) where
 
---import Data.Map.Strict (Map)
---import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import Flow
 
 --data File = RegularFile String Int | Directory String [File]
 --type Directory = Map String [RegularFile]  -- name, files
-type Directory = (String, [RegularFile])
-type RegularFile = (String, Int)  -- name, size
+type Directory = Map String Int -- filename -> size
+--type RegularFile = (String, Int)  -- name, size
 
 day07_1 :: [String] -> Int
 day07_1 input = do
@@ -16,26 +16,23 @@ day07_1 input = do
 
 buildDirectoryTree :: [String] -> Directory
 --buildDirectoryTree = doBuildDirectoryTree (Map.fromList [("/", [])]) []
-buildDirectoryTree = doBuildDirectoryTree ("/", []) []
+buildDirectoryTree = doBuildDirectoryTree Map.empty
 
-doBuildDirectoryTree :: Directory -> [Directory] -> [String] -> Directory -- root -> current path -> input -> final directory tree
-doBuildDirectoryTree root _ [] = root
-doBuildDirectoryTree root path (x:xs)
+doBuildDirectoryTree :: Directory -> [String] -> Directory -- root -> current path -> input -> final directory tree
+doBuildDirectoryTree root [] = root
+doBuildDirectoryTree root (x:xs)
   | x == "$ cd /" = do  -- I do hope this is just the first command and happens just once
-    let newRoot = ("/", [])
-    doBuildDirectoryTree newRoot [newRoot] xs
-  | x == "$ ls" = doBuildDirectoryTree root path xs -- nothing to do
+    doBuildDirectoryTree root xs
+  | x == "$ ls" = doBuildDirectoryTree root xs -- nothing to do
 --  | x == "$ cd .." = doBuildDirectoryTree root (tail path) xs
   | otherwise = do
     let y = words x
     if head y == "dir"
-    then error "Unsupported yet"  -- not yet supported
-    else do -- should be pattern: "size fileName"
-      let fs = read (head y) :: Int
-      let filename = y !! 1
-      let file = (filename, fs)
-      let newRoot = ("/", [file])
-      doBuildDirectoryTree newRoot [newRoot] xs
+      then error "Unsupported yet"  -- not yet supported
+      else do -- should be pattern: "size fileName"
+        let fs = read (head y) :: Int
+        let filename = y !! 1
+        doBuildDirectoryTree (Map.insert filename fs root) xs
 
 --  | Just dirName <- stripPrefix "$ cd " x = doBuildDirectoryTree root ((head path) !! dirName) xs
 --  | otherwise = error "Unsupported yet"
@@ -47,7 +44,7 @@ findDirs dir = [dir]
 --findDirs (RegularFile _ _) = []
 
 size :: Directory -> Int
-size (_, files) = map snd files |> sum
+size dir = Map.elems dir |> sum
 --size dir = Map.elems dir |> concat |> map snd |> sum
 --size (Directory name [files]) = 0
 --size (RegularFile _ s) = s
