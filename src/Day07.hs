713a21e24ca39eb65b80b1f03ebc5b88cbb71449
Jacek Bilski <jacek@bilski.tech>
2023-01-09 17:25:47 +0100

    Messing around part 11 - some cleanups.

--- src/Day07.hs
+++ src/Day07.hs
@@ -7,22 +7,19 @@ import Flow
 data File = RegularFile Int | Directory (Map String File)
 
 day07_1 :: [String] -> Int
-day07_1 input = do
-  let root = buildDirectoryTree input
-  let dirs = findDirs root
-  let smallDirs = filter (\d -> size d <= 100000) dirs
-  map size smallDirs |> sum
+day07_1 input = findDirs (buildDirectoryTree input) |> filter (\d -> size d <= 100000) |> map size |> sum
 
 buildDirectoryTree :: [String] -> File
 --buildDirectoryTree = doBuildDirectoryTree (Map.fromList [("/", [])]) []
 buildDirectoryTree = doBuildDirectoryTree (Directory Map.empty)
 
 doBuildDirectoryTree :: File -> [String] -> File -- root -> current path -> input -> final directory tree
+doBuildDirectoryTree (RegularFile _) _ = error "/ cannot be a regular file"
 doBuildDirectoryTree root [] = root
-doBuildDirectoryTree root (x:xs)
-  | x == "$ cd /" = do  -- I do hope this is just the first command and happens just once
-    doBuildDirectoryTree root xs
-  | x == "$ ls" = doBuildDirectoryTree root xs -- nothing to do
+doBuildDirectoryTree (Directory root) (x:xs)
+  | x == "$ cd /" =  -- I do hope this is just the first command and happens just once
+    doBuildDirectoryTree (Directory root) xs
+  | x == "$ ls" = doBuildDirectoryTree (Directory root) xs -- nothing to do
 --  | x == "$ cd .." = doBuildDirectoryTree root (tail path) xs
   | otherwise = do
     let y = words x
@@ -32,15 +29,7 @@ doBuildDirectoryTree root (x:xs)
         let fs = read (head y) :: Int
         let filename = y !! 1
         let file = RegularFile fs
-        case root of
-          Directory dir -> doBuildDirectoryTree (Directory (Map.insert filename file dir)) xs
-          RegularFile _ -> error "/ cannot be a regular file"
---        Directory (Map name [files]) <- root
---        doBuildDirectoryTree (Map.insert filename file root) xs
-
---  | Just dirName <- stripPrefix "$ cd " x = doBuildDirectoryTree root ((head path) !! dirName) xs
---  | otherwise = error "Unsupported yet"
---doBuildDirectoryTree _ _ _ = ("/", Just ("x", 123))
+        doBuildDirectoryTree (Directory (Map.insert filename file root)) xs
 
 findDirs :: File -> [File]
 findDirs dir = [dir]
@@ -50,7 +39,6 @@ findDirs dir = [dir]
 size :: File -> Int
 size (Directory dir) = Map.elems dir |> map size |> sum
 size (RegularFile s) = s
---size (Map name files) = Map.elems dir |> sum
 
 day07_2 :: [String] -> Int
 day07_2 _ = -1
