6c567205b926a9803b071ffc3761ef9236460e99
Jacek Bilski <jacek@bilski.tech>
2023-01-09 18:13:47 +0100

    Messing around part 12 - extracted `addFile`. `doBuildDirectoryTree` now also takes path.

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,5 +1,6 @@
 module Day07 (day07_1, day07_2) where
 
+import Data.List (isPrefixOf)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Flow
@@ -10,26 +11,34 @@ day07_1 :: [String] -> Int
 day07_1 input = findDirs (buildDirectoryTree input) |> filter (\d -> size d <= 100000) |> map size |> sum
 
 buildDirectoryTree :: [String] -> File
---buildDirectoryTree = doBuildDirectoryTree (Map.fromList [("/", [])]) []
-buildDirectoryTree = doBuildDirectoryTree (Directory Map.empty)
+buildDirectoryTree = doBuildDirectoryTree (Directory Map.empty) []
 
-doBuildDirectoryTree :: File -> [String] -> File -- root -> current path -> input -> final directory tree
-doBuildDirectoryTree (RegularFile _) _ = error "/ cannot be a regular file"
-doBuildDirectoryTree root [] = root
-doBuildDirectoryTree (Directory root) (x:xs)
+doBuildDirectoryTree :: File -> [String] -> [String] -> File -- root -> current path -> input -> final directory tree
+--doBuildDirectoryTree (RegularFile _) _ = error "/ cannot be a regular file"
+doBuildDirectoryTree root _ [] = root
+doBuildDirectoryTree (Directory root) path (x:xs)
   | x == "$ cd /" =  -- I do hope this is just the first command and happens just once
-    doBuildDirectoryTree (Directory root) xs
-  | x == "$ ls" = doBuildDirectoryTree (Directory root) xs -- nothing to do
---  | x == "$ cd .." = doBuildDirectoryTree root (tail path) xs
+    doBuildDirectoryTree (Directory root) [] xs
+  | x == "$ ls" = doBuildDirectoryTree (Directory root) path xs -- nothing to do
+  | "$ cd " `isPrefixOf` x = do
+    let dirName = drop 5 x
+    case dirName of
+      ".." -> doBuildDirectoryTree (Directory root) (tail path) xs
+      otherwise -> doBuildDirectoryTree (Directory root) (dirName : path) xs
   | otherwise = do
     let y = words x
     if head y == "dir"
-      then error "Unsupported yet"  -- not yet supported
+      then do
+        let filename = y !! 1
+        doBuildDirectoryTree (addFile filename (Directory Map.empty) [] (Directory root)) path xs
       else do -- should be pattern: "size fileName"
         let fs = read (head y) :: Int
         let filename = y !! 1
         let file = RegularFile fs
-        doBuildDirectoryTree (Directory (Map.insert filename file root)) xs
+        doBuildDirectoryTree (addFile filename file [] (Directory root)) path xs
+
+addFile :: String -> File -> [String] -> File -> File
+addFile filename file path (Directory root) = Directory (Map.insert filename file root)
 
 findDirs :: File -> [File]
 findDirs dir = [dir]
