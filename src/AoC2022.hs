ccd6060674343184b1d61242b84027723d848af7
Jacek Bilski <jacek@bilski.tech>
2023-01-16 16:23:48 +0100

    Day 09 - infrastructure.

--- src/AoC2022.hs
+++ src/AoC2022.hs
@@ -1,5 +1,5 @@
 module AoC2022 (solve, day01_1, day01_2, day02_1, day02_2, day03_1, day03_2, day04_1, day04_2, day05_1, day05_2,
-  day06_1, day06_2, day07_1, day07_2, day08_1, day08_2) where
+  day06_1, day06_2, day07_1, day07_2, day08_1, day08_2, day09_1, day09_2) where
 
 import Flow
 import System.IO
@@ -12,6 +12,7 @@ import Day05
 import Day06
 import Day07
 import Day08
+import Day09
 
 solve :: FilePath -> ([String] -> a) -> IO a
 solve fileName day = do
---
+++ src/Day09.hs
@@ -0,0 +1,7 @@
+module Day09 (day09_1, day09_2) where
+
+day09_1 :: [String] -> Int
+day09_1 _ = undefined
+
+day09_2 :: [String] -> Int
+day09_2 _ = undefined
