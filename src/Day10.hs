23ad2f9f39c7626bb2826c5e5fc156769b66cfe0
Jacek Bilski <jacek@bilski.tech>
2023-01-17 08:33:29 +0100

    Day 10 - infrastructure.

---
+++ src/Day10.hs
@@ -0,0 +1,7 @@
+module Day10 (day10_1, day10_2) where
+
+day10_1 :: [String] -> Int
+day10_1 _ = undefined
+
+day10_2 :: [String] -> Int
+day10_2 _ = undefined
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -40,4 +40,6 @@ test_aoc = testGroup "Advent of Code 2022" [
    ,labeledDayTestCase "day 08 part 2" day08_2 "inputs/day08.input" 259308
    ,labeledDayTestCase "day 09 part 1" day09_1 "inputs/day09.input" 6357
    ,labeledDayTestCase "day 09 part 2" day09_2 "inputs/day09.input" 2627
+   ,labeledDayTestCase "day 10 part 1" day10_1 "inputs/day10.input" (-1)
+   ,labeledDayTestCase "day 10 part 2" day10_2 "inputs/day10.input" (-1)
   ]
