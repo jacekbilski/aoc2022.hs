ccd6da2d3e97de4ca757a82562fff30f0d48ae94
Jacek Bilski <jacek@bilski.tech>
2023-01-23 15:03:25 +0100

    Playing around, `splitOn ','` will be tricky.

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,7 +1,34 @@
 module Day13 (day13_1, day13_2) where
 
-day13_1 :: [String] -> Int
-day13_1 _ = undefined
+import Data.List.Split (chunksOf, splitOn)
+import Flow
+
+data D = N Int | L [D]
+type Packet = D
+type Pair = (Packet, Packet)
+
+instance Show D where
+  show (N x) = show x
+  show (L x) = show x
+
+day13_1 :: [String] -> String
+day13_1 input = do
+  let pairs = parseInput input
+  show pairs
+
+parseInput :: [String] -> [Pair]
+parseInput input = chunksOf 3 input |> map parsePair
+
+parsePair :: [String] -> Pair
+parsePair input = (parsePart (head input), parsePart (input !! 1))
+
+parsePart :: String -> D
+parsePart part
+  | head part == '[' = take (length part - 2) (tail part) |> split ',' |> map parsePart |> L
+  | otherwise = N (read part)
+
+split :: Char -> String -> [String]
+split char = splitOn [char]
 
 day13_2 :: [String] -> Int
 day13_2 _ = undefined
