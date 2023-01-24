8aecce01d7f2d9e1dca40538430dbd4f519f3572
Jacek Bilski <jacek@bilski.tech>
2023-01-24 16:16:52 +0100

    Day 15 - infrastructure.

--- src/AoC2022.hs
+++ src/AoC2022.hs
@@ -1,6 +1,6 @@
 module AoC2022 (solve, readInput, day01_1, day01_2, day02_1, day02_2, day03_1, day03_2, day04_1, day04_2, day05_1, day05_2,
   day06_1, day06_2, day07_1, day07_2, day08_1, day08_2, day09_1, day09_2, day10_1, day10_2, day11_1, day11_2,
-  day12_1, day12_2, day13_1, day13_2, day14_1, day14_2) where
+  day12_1, day12_2, day13_1, day13_2, day14_1, day14_2, day15_1, day15_2) where
 
 import System.IO
 
@@ -18,6 +18,7 @@ import Day11
 import Day12
 import Day13
 import Day14
+import Day15
 
 solve :: FilePath -> ([String] -> a) -> IO a
 solve fileName day = do
---
+++ src/Day15.hs
@@ -0,0 +1,9 @@
+module Day15 (day15_1, day15_2) where
+
+day15_1 :: [String] -> Int
+day15_1 input = do
+  undefined
+
+day15_2 :: [String] -> Int
+day15_2 input = do
+  undefined
